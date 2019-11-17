5a375e4e3fec2741ad9ded58eda49f6825055085
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-17 17:45:55 -0500

    router experiment

--- mern-project-react/src/App.js
+++ mern-project-react/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import React, { Component } from 'react';
 // import axios from '../package.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,24 +7,24 @@ import EditMovie from './components/EditMovie';
 import CreateMovie from './components/CreateMovie';
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  // constructor(props){
+  //   super(props);
 
-    this.state = { 
+  //   this.state = { 
 
-    };
-  }
+  //   };
+  // }
 
 render() {
   return (
-    <Router>
+
     <div className="container">
       <h2>Mern Stack shit hott</h2>
     <Route path="/" exact component={MoviesList} />
     <Route path="/edit/:id" component={EditMovie} />
     <Route path="/create" component={CreateMovie} /> 
     </div> 
-    </Router>
+  
   )
 };
   }
