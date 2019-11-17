a44ead0b7bc36de94f8644c35f5c9560349d2d1d
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-17 18:51:02 -0500

    routes work initially

--- mern-project-react/src/App.js
+++ mern-project-react/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import React, { Component } from 'react';
 // import axios from '../package.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,13 +17,20 @@ class App extends Component {
 
 render() {
   return (
-
+    <Router>
     <div className="container">
-      <h2>Mern Stack shit hott</h2>
+   
+              <Link to="/" className="nav-link">Movies</Link>
+           
+              <Link to="/create" className="nav-link">Create Movie</Link>
+         
+              <Link to="/edit/:id" className="nav-link">Edit Movies</Link>
+     
     <Route path="/" exact component={MoviesList} />
     <Route path="/edit/:id" component={EditMovie} />
     <Route path="/create" component={CreateMovie} /> 
     </div> 
+    </Router>
   
   )
 };
