1315a7c25f883e0f2325f46ea8452433ba1964c0
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-17 19:38:46 -0500

    create movie button

--- mern-project-react/src/components/CreateMovie.js
+++ mern-project-react/src/components/CreateMovie.js
@@ -1,10 +1,105 @@
 import React, {Component} from 'react';
 
 export default class CreateMovie extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeMovieTitle = this.onChangeMovieTitle.bind(this);
+        this.onChangeMovieOverview = this.onChangeMovieOverview.bind(this);
+        this.onChangeMovieReleaseDate = this.onChangeMovieReleaseDate.bind(this);
+        this.onChangeMovieVoteAverage = this.onChangeMovieVoteAverage.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+
+
+        this.state = {
+            title: String,
+            overview: String,
+            releaseDate: String,
+            voteAverage: Number
+        }
+    }
+
+    onChangeMovieTitle(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+
+    onChangeMovieOverview(e) {
+        this.setState({
+            overview: e.target.value
+        });
+    }
+
+    onChangeMovieReleaseDate(e) {
+        this.setState({
+            releaseDate: e.target.value
+        });
+    }
+
+    onChangeMovieVoteAverage(e) {
+        this.setState({
+            voteAverage: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+            console.log(`Form Submitted:`);
+            console.log(`Movie Title: ${this.state.title}`);
+            console.log(`Movie Overview: ${this.state.overview}`);
+            console.log(`Movie Release Date: ${this.state.releaseDate}`);
+            console.log(`Movie Vote Average: ${this.state.voteAverage}`);
+//on submit logic
+
+        this.setState({
+            title: String,
+            overview: String,
+            releaseDate: String,
+            voteAverage: Number
+        })
+    }
+
+
     render() {
         return (
-            <div>
-                <p>Welcome to Create Movie Zone.</p>
+            <div style={{marginTop: 20}}>
+                <h3>Create New Movie</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Title: </label>
+                        <input type="text"
+                               className="form-control"
+                               value={this.state.title}
+                               onChange={this.onChangeMovieTitle}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Overview: </label>
+                        <input type="text"
+                               className="form-control"
+                               value={this.state.overview}
+                               onChange={this.onChangeMovieOverview}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Release Date: </label>
+                        <input type="text"
+                               className="form-control"
+                               value={this.state.releaseDate}
+                               onChange={this.onChangeMovieReleaseDate}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Vote Average: </label>
+                        <input type="text"
+                               className="form-control"
+                               value={this.state.voteAverage}
+                               onChange={this.onChangeMovieVoteAverage}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Movie" className="btn btn-primary"/>
+                    </div> 
+                </form>
             </div>
         )
     }
